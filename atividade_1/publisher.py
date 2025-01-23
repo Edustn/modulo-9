3e3d7782113338718776e2b9490933cccb9c4933
Edustn <eduardo.santos@sou.inteli.edu.br>
2025-01-22 21:22:23 -0300

    feat: insere dados dos sensores

--- atividade_1/publisher.py
+++ atividade_1/publisher.py
@@ -1,16 +1,26 @@
 import paho.mqtt.client as mqtt
 import time
 import random
+import json
 
 # Função que contém todos os dados do datashet
 
 def datasheet():
+    solar_radiation = round(random.uniform(0,1280), 2)
+    spectral_range = round(random.uniform(300, 1100), 1)
     
-    return {
+    data = {
         "time": time.strftime("%H:%M:%S"),
+        "solar_radiation": solar_radiation,
+        "unit_solar_radiation": "W/m^2",
+
+        "spectral_range": spectral_range,
+        "unit_spectral_range": "nm"
+
 
     }
-
+    return json.dumps(data)
+    
 # Criação do cliente para o publisher
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "python_publisher")
